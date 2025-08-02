443bf36aa4a9d3f674aadd54ee8230e3f39e7285
saptarshi-62 <saptarshidey6290186223@gmail.com>
2025-08-02 20:25:09 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 // filepath: f:\website\inote\backend\db.js
 const mongoose = require('mongoose');
-const mongoURI = "mongodb+srv://sapdey62:fhYFdn3v2PWPOo7n@cluster0.c8dotpg.mongodb.net/iNote?retryWrites=true&w=majority";
+const mongoURI = "mongodb+srv://sapdey62:fhYFdn3v2PWPOo7n@cluster0.c8dotpg.mongodb.net/iNote?retryWrites=true&w=majority"
 //process.env.MONGO_URI //||"mongodb://localhost:27017/inote";
 
 const connectToMongo = () => {
